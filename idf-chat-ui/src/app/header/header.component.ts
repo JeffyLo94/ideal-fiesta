aa9bb1387b06dedf216c1010327a70e64f87062f
Jeffrey Lo <jeffylo94@gmail.com>
2019-05-04 23:09:05 -0700

    Header for authentication

--- idf-chat-ui/src/app/header/header.component.ts
+++ idf-chat-ui/src/app/header/header.component.ts
@@ -1,19 +1,110 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { AuthService } from '../services/auth.service';
+import { Component, OnInit, Input, ViewEncapsulation, Inject, EventEmitter, Output } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+// import { User } from 'firebase';
 
+export interface HeaderLink {
+  text: string;
+  path: string;
+  icon: string;
+}
+export interface HeaderUser {
+  avatarUrl: string;
+  name: string;
+  // More to be added
+}
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  // encapsulation: ViewEncapsulation.None
 })
 export class HeaderComponent implements OnInit {
-  @Input() links: { text: string, path: string }[];
+  @Input() links: HeaderLink[];
   @Input() displayLogo: boolean;
-  @Input() view: string;
+  @Input() label: string;
+  @Input() user?: HeaderUser;
+  @Input() hasLogin?: boolean = false;
+  @Output() login?: EventEmitter<any> = new EventEmitter();
+  @Output() logout?: EventEmitter<any> = new EventEmitter();
 
-  constructor( private readonly authService: AuthService) { }
+  // idemiaLogo = 'assets/idemiaLogo/IDEMIA-Logo-White-Transparent.svg';
+
+  private readonly FS_ENABLE: string = 'fullscreen';
+  private readonly FS_DISABLE: string = 'fullscreen_exit';
+
+  private elem;
+  fullscreenIcon = 'fullscreen';
+
+
+  constructor(@Inject(DOCUMENT) private readonly doc: any) {
+    this.fullscreenIcon = 'fullscreen';
+   }
 
   ngOnInit() {
+    this.elem = this.doc.documentElement;
+
+    this.elem.webkitIsFullScreen ? (this.fullscreenIcon = this.FS_DISABLE) : (this.fullscreenIcon = this.FS_ENABLE);
+
+    console.log(this.fullscreenIcon);
+  }
+
+  handleLogin() {
+    console.log('login pressed - TODO change when authentication implemented');
+    //emit event here
+    this.login.emit();
+  }
+
+  handleLogout() {
+    console.log('logout pressed - TODO change when authentication implemented');
+    this.login.emit();
+  }
+
+  toggleFullscreen() {
+    if (this.doc.webkitIsFullScreen) {
+      console.log('clicked to exit');
+      this._exitFullscreen();
+      this.fullscreenIcon = this.FS_ENABLE;
+    } else {
+      console.log('clicked to full: ', this.doc.webkitIsFullScreen);
+      this._activateFullscreen();
+      this.fullscreenIcon = this.FS_DISABLE;
+    }
+  }
+
+  /**
+   * Activate Full Screen Mode.
+   */
+  private _activateFullscreen() {
+    if (this.elem.requestFullscreen) {
+      this.elem.requestFullscreen();
+    } else if (this.elem.mozRequestFullScreen) {
+      /* Firefox */
+      this.elem.mozRequestFullScreen();
+    } else if (this.elem.webkitRequestFullscreen) {
+      /* Chrome, Safari and Opera */
+      this.elem.webkitRequestFullscreen();
+    } else if (this.elem.msRequestFullscreen) {
+      /* IE/Edge */
+      this.elem.msRequestFullscreen();
+    }
+  }
+
+  /**
+   * Exits Full Screen Mode.
+   */
+  private _exitFullscreen() {
+    if (this.doc.exitFullscreen) {
+      this.doc.exitFullscreen();
+    } else if (this.doc.mozCancelFullScreen) {
+      /* Firefox */
+      this.doc.mozCancelFullScreen();
+    } else if (this.doc.webkitExitFullscreen) {
+      /* Chrome, Safari and Opera */
+      this.doc.webkitExitFullscreen();
+    } else if (this.doc.msExitFullscreen) {
+      /* IE/Edge */
+      this.doc.msExitFullscreen();
+    }
   }
 
 }
--- idf-chat-ui/src/app/services/account-creation.service.ts
+++ idf-chat-ui/src/app/services/account-creation.service.ts
@@ -1,9 +1,40 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
+import { User } from '../chat-objects.model';
+import { FirestoreService } from './firestore.service';
+import { AuthService } from './auth.service';
+import { Observable, BehaviorSubject, Subject } from 'rxjs';
+import { takeUntil, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AccountCreationService {
+export class AccountCreationService implements OnDestroy {
+  user: User;
 
-  constructor() { }
+  userListData: BehaviorSubject<User[]> = new BehaviorSubject([]);
+  userList$: Observable<User[]> = this.userListData.asObservable();
+
+  private destroy$: Subject<boolean> = new Subject<boolean>();
+
+  constructor(private fs: FirestoreService, private as: AuthService) {
+
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  requestUsersFromDatabase() {
+    return this.fs.getUsers().pipe(
+      takeUntil(this.destroy$),
+      map( (users) => {
+        return users.map( (a) => {
+            const userData = a.payload.doc.data();
+            const id = a.payload.doc.id;
+          }
+        );
+      })
+    ).subscribe();
+  }
 }
